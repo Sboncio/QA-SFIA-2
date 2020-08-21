54a6196e35871fc9f274a2f6b36791352704ae30
Sam <samuelasquith@btinternet.com>
2020-08-21 14:52:12 +0000

    Progress on service 4

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -7,6 +7,12 @@ from application import app
 def home():
     return render_template('home.html')
 
-#@app.route('/weather')
-#def getWeather():
-    #response = requests.get('http://service2:5001/weather/get')
+@app.route('/weather')
+def getWeather():
+    response = requests.get('http://service2:5001/weather/get')
+    weather = response.text
+
+@app.route('/speed')
+def getSpeed():
+    response = requests.get('http://service3:5002/speed/get')
+    speed = response.text
