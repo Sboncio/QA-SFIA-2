1a62b43f5eeea8fc457070de229951980f87c659
sboncio <samuelasquith@btinternet.com>
2020-08-22 16:24:04 +0100

    Service 4

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request
-
+import requests
 from application import app
 
 @app.route('/')
@@ -10,9 +10,21 @@ def home():
 @app.route('/weather')
 def getWeather():
     response = requests.get('http://service2:5001/weather/get')
-    weather = response.text
+    return response.text
 
 @app.route('/speed')
 def getSpeed():
     response = requests.get('http://service3:5002/speed/get')
-    speed = response.text
+    return response.text
+
+@app.route('/send_data', methods=['POST'])
+def sendData(weather, speed):
+    response = requests.post('http://service4:5003/return/data', json={'weather': weather, 'speed': speed})
+    return response.text
+
+@app.route('/communicate')
+def communicate():
+    weather = getWeather()
+    speed = getSpeed()
+    result = sendData(weather, speed)
+    return render_template('results.html', weather=weather,speed=speed,result=result)
--- service4/app.py
+++ service4/app.py
@@ -3,9 +3,8 @@ app = Flask(__name__)
 
 @app.route('/return/data', methods=['POST'])
 def returnData():
-    data_sent = request.data.decode('utf-8')
-    upper_data = data_sent.upper()
-    return Response("This is the data you sent to the API but in upper case: " + upper_data, mimetype='text/plain')
+    data_sent = request.get_json()
+    return Response("This is the data you sent to the API: " + data_sent['weather'], mimetype='text/plain')
 
     
 if __name__ == '__main__':
