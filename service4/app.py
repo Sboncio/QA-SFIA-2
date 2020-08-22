785eeda71060f2936eb586c46e1598ca7e3e22b7
sboncio <samuelasquith@btinternet.com>
2020-08-22 17:21:21 +0100

    Service 4 logic

--- service4/app.py
+++ service4/app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 @app.route('/return/data', methods=['POST'])
 def returnData():
     data_sent = request.get_json()
-    return Response("This is the data you sent to the API: " + data_sent['weather'], mimetype='text/plain')
+    if data_sent['speed'] == 'Fast':
+        result = 'Slow to appropriate speed'
+    elif (data_sent['speed'] == 'Slow' and data_sent['weather'] != 'Sun') or (data_sent['speed'] == 'Average' and data_sent['weather'] == 'Rain'):
+        result = 'Proceed with caution'
+    elif data_sent['speed'] == 'Average' and (data_sent['weather'] == 'Snow' or data_sent['weather'] == 'Frost'):
+        result = 'Slow down'
+    elif data_sent['speed'] == 'Average' and data_sent['weather'] == 'Sun':
+        result = 'Continue'
+    else:
+        result = 'Drive with care at appropriate speed'
+    return Response(result, mimetype='text/plain')
 
     
 if __name__ == '__main__':
