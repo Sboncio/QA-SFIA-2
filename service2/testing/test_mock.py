f63b3046ba24be8e36b16cce3bbbb7e7592d7a73
Sam <samuelasquith@btinternet.com>
2020-08-23 15:29:57 +0000

    Service1 and Service2 testing 100% coverage

---
+++ service2/testing/test_mock.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+from application import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_random_weather(self):
+        with patch('application.routes.gen_random') as g:
+            g.return_value.data = 0
+
+            response = self.client.get(url_for('get_weather'))
+            self.assertIn(b'Sun', response.data)
+
+    def test_random_num(self):
+        with patch('random.randint') as g:
+            g.return_value.data = str(1)
+            response = self.client.get(url_for('gen_random'))
+            self.assertIn(b'1', response.data)
