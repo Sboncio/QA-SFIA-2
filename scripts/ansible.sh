33532fd473e8f6be75b3cd311dfeab7ad30a9cc5
sboncio <samuelasquith@btinternet.com>
2020-08-28 17:44:02 +0000

    Update scripts

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -5,6 +5,6 @@ echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 . ~/.bashrc
 sudo apt install python3-pip
 ## install ansible with pip
-pip3 install --user ansible
-sudo apt install ansible
+pip3 install ansible
+#sudo apt install ansible
 ansible-playbook -i ./ansible/inventory ./ansible/playbook.yaml
