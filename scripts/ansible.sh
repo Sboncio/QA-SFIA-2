19bdc4822c08ec6d2f5f999479697f47c2012ae4
sboncio <samuelasquith@btinternet.com>
2020-08-28 17:30:38 +0000

    Update scripts

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 # make sure ~/.local/bin exists and is on your PATH
 sudo mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-sudo . ~/.bashrc
-sudo apt install pip
+. ~/.bashrc
+sudo apt install python3-pip
 ## install ansible with pip
-pip install --user ansible
+pip3 install --user ansible
 
 ansible-playbook -i inventory ansible/playbook.yaml
