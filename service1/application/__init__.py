3d51d0f52dcd9806144d0fa9784f16cf4fd65584
sboncio <samuelasquith@btinternet.com>
2020-08-31 11:57:35 +0000

    Updates

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY_DB'))
-app.config['SQLALCHEMY_DATABASE_URI']= 
+app.config['SQLALCHEMY_DATABASE_URI']= "mysql+pymysql://root:hannahandsam1@mysql:3306/sfia2"
 #str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 
