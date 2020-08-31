28289ccb9a97ac044fd02b9313f7c5b11406d17d
sboncio <samuelasquith@btinternet.com>
2020-08-31 10:05:56 +0000

    Updates

--- service1/application/__init__.py
+++ service1/application/__init__.py
@@ -5,10 +5,8 @@ import os
 # create a new instance of Flask and store it in app 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "secret_key"
-#str(os.getenv('SECRET_KEY_DB'))
-app.config['SQLALCHEMY_DATABASE_URI']= "mysql+pymysql://root:hannahandsam1@mysql:3306/sfia2"
-#str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY_DB'))
+app.config['SQLALCHEMY_DATABASE_URI']= str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 
 # import the ./application/routes.py file
